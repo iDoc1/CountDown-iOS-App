5942989c27957d692ac71417156506c91425bcff
Ian Docherty <ianardocherty@gmail.com>
2023-09-09 17:06:42 -0700

    Initial Commit

---
+++ CountDown/CountDownApp.swift
@@ -0,0 +1,17 @@
+//
+//  CountDownApp.swift
+//  CountDown
+//
+//  Created by Ian Docherty on 9/9/23.
+//
+
+import SwiftUI
+
+@main
+struct CountDownApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
